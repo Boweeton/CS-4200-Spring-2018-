30bfe2f7092d6fc84ba684612447ec3c449b716a
LAPTOP-UN3TDJJ3\Boweeton <lsoderst@uccs.edu>
2018-02-21 07:00:11 -0700

    Built more framework

---
+++ Project_1/Project_1/MemoryLookasideBuffer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class MemoryLookasideBuffer
+    {
+        #region Fields
+
+
+
+        #endregion
+
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
